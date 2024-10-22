e5a46d7c2aaf07da544c666eb94189d7adcedc51
WEBDEVTEACH <teachwebdev@gmail.com>
2024-10-22 18:30:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let customerNo = ["TBK-NMI-15-0027"]
+
+/*
+[
+  {
+  customerNo:"TBK-NMI-15-0027"
+  }
+]
+*/
+
+customerNo = customerNo.map(el=>({customerNo:el}))
+
+console.log(customerNo)
+
+//map
+//filter
+//reduce
